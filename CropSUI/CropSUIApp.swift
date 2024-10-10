20a46da12d9e3297697e945f721663229fcf3a50
Salakhoff <salakhoff.dev@gmail.com>
2024-10-10 14:17:20 +0300

    Initial Commit

---
+++ CropSUI/CropSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CropSUIApp.swift
+//  CropSUI
+//
+//  Created by Ильфат Салахов on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CropSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
